ffbf6f631f28dc0e8e1c98c1c445ac414f157a6e
Jensen Lawrence <jptlawrnce@gmail.com>
2022-04-15 14:02:46 -0700

    Update Laguerre polynomials

--- src/laguerre.jl
+++ src/laguerre.jl
@@ -3,260 +3,104 @@
 # ----------------------------------------------------------------------------------------------------------------------
 
 """
-    laguerre_coefficients(n)
+    laguerre_L_f(n, x)
 
-Calculates the coefficients of the nth Laguerre polynomial using the power
-series representation of Laguerre polynomials,
-
-``L_n(x) = \\sum_{k=0}^n (-1)^k \\binom{n}{k} \frac{x^k}{k!}``
-
-Coefficients are ordered from lowest to highest degree for compatibility
-with `evalpoly`.
+Evaluates the nth Laguerre polynomial at x ∈ ℝ.
 """
-function laguerre_coefficients(n::Int)
-    coefficients = zeros(n + 1)
-    for k ∈ 0:n
-        if k ≤ 20
-            coefficients[k+1] = ((-1)^k)/factorial(k) * binomial(n, k)
-        else
-            coefficients[k+1] = ((-1)^k)/gamma(k + 1) * binomial(n, k)
-        end
+function laguerre_L_f(n::Int, x::R) where {R<:Real}
+    L = sum([genbinom(n, k) * ((-1)^k)/genfac(k) * x^k for k ∈ 0:n])
+    return L
+end
+
+"""
+    laguerre_L_w(x)
+
+Evaluates the orthogonality weight function of the nth Laguerre polynomial 
+at x ∈ ℝ.
+"""
+function laguerre_L_w(x::R) where {R<:Real}
+    return exp(-x)
+end
+
+"""
+    LaguerreL(n)
+
+Representation of the nth Laguerre polynomial, Lₙ, where n ∈ ℤ⁺.
+
+Available methods:
+- `LaguerreL(n).n` returns n (the polynomial degree).
+- `LaguerreL(n).f` returns the polyomial function.
+- `LaguerreL(n).w` returns the orthogonality weight function.
+- `LaguerreL(n).I` returns the orthogonality interval.
+
+`LaguerreL(n)` is directly callable, so Lₙ(x) can be evaluated using 
+`LaguerreL(n)(x)`. Lₙ(x) can also be evaluated using `LaguerreL(n).f(x)`.
+Similarly, the orthogonality weight function w(x) can be evaluated using 
+`LaguerreL(n).w(x)`.
+
+Aliases: `L`, `Laguerre`.
+"""
+struct LaguerreL <: AbstractLaguerre 
+    n::Int 
+    f 
+    w 
+    I 
+
+    function LaguerreL(n)
+        n ≥ 0 || error("n must be non-negative.")
+        f = x -> laguerre_L_f(n, x)
+        w = x -> laguerre_L_w(x)
+        I = (0, Inf)
+        new(n, f, w, I)
     end
-    return SVector{n+1}(coefficients)
+end
+
+# Make LaguerreL(n) callable 
+function (L::LaguerreL)(x::R) where {R<:Real}
+    return L.f(x)
+end
+
+"""
+    L(n)
+
+Alias of `LaguerreL`.
+"""
+function L(n::Int)
+    return LaguerreL(n)
 end
 
 """
     Laguerre(n)
 
-Representation of the nth Laguerre polynomial. Available methods include:
-- `n`: the polynomial degree 
-- `coefficients`: the polynomial coefficients 
-- `polynomial`: the polynomial function 
-- `weight`: the associated Laguerre polynomial weight function, ``w(x) = e^{-x}``
-- `interval`: the associated Laguerre polynomial orthogonality interval, ``[0, \\infty)``
+Alias of `LaguerreL`.
 """
-struct Laguerre <: AbstractLaguerre
-    n::Int 
-    coefficients
-    polynomial
-    weight
-    interval
-
-    function Laguerre(n)
-        coefficients = laguerre_coefficients(n)
-        polynomial = x -> evalpoly(x, coefficients)
-        weight = x -> exp(-x)
-        interval = (0, Inf)
-        new(n, coefficients, polynomial, weight, interval)
-    end
+function Laguerre(n::Int)
+    return LaguerreL(n)
 end
-
-"""
-    LaguerreL(n, x)
-    LaguerreL(x; n)
-
-Evaluates the nth Laguerre polynomial at x ∈ ℝ.
-"""
-function LaguerreL(n::Int, x::T) where {T<:Real}
-    return Laguerre(n).polynomial(x)
-end
-LaguerreL(x; n) = LaguerreL(n, x)
-
-"""
-    L(n, x)
-    L(x; n)
-
- Evaluates the nth Laguerre polynomial at x ∈ ℝ.
-"""
-L(n, x) = LaguerreL(n, x)
-L(x; n) = LaguerreL(x; n)
-
-"""
-    coefficients(L)
-
-Determines the coefficients of a given Laguerre polynomial.
-"""
-coefficients(L::Laguerre) = L.coefficients 
-
-"""
-    weight(L)
-
-Determines the weight function of a given Laguerre polynomial.
-"""
-weight(L::Laguerre) = L.weight 
-
-"""
-    interval(L)
-
-Determines the interval of orthogonality of a given Laguerre polynomial.
-"""
-interval(L::Laguerre) = L.interval
 
 """
     innerproduct(L1, L2)
 
-Calculates the inner product of the Laguerre polynomials `L1` and `L2`
-using the orthogonality relation
-
-``\\int_0^{\\inftyty} L_m^(x) L_n^(x) e^{-x} \\: \\mathrm{d}x = \\delta_{nm}``
+Returns the inner product of `L1` and `L2` using the orthogonality relation 
+for Laguerre polynomials.
 """
-function innerproduct(L1::Laguerre, L2::Laguerre)
+function innerproduct(L1::LaguerreL, L2::LaguerreL)
     n, m = L1.n, L2.n 
-    if n != m 
-        return 0
-    else
-        return 1
-    end
+    return δ(n, m)
 end
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Associated Laguerre Polynomials
+# General
 # ----------------------------------------------------------------------------------------------------------------------
 
 """
-    associated_laguerre_coefficients(n, α)
+    roots(F)
 
-Calculates the coefficients of the (n, α) associated Laguerre polynomial using
-the power series representation of associated Laguerre polynomials,
-
-``L_n^{(\\alpha)}(x) = \\sum_{k=0}^n (-1)^k \\binom{n + α}{n - k} \frac{x^k}{k!}``
-
-Coefficients are ordered from lowest to highest degree for compatibility
-with `evalpoly`.
+Returns the roots (zeros) of `F`.
 """
-function associated_laguerre_coefficients(n::Int, α::T) where {T<:Real}
-    if α == 0
-        return laguerre_coefficients(n)
-    else
-        coefficients = zeros(n + 1)
-        for k ∈ 0:n
-            if k ≤ 20
-                if typeof(α) <: Int
-                    coefficients[k+1] = ((-1)^k)/factorial(k) * binomial(n + α, n - k)
-                else
-                    coefficients[k+1] = ((-1)^k)/factorial(k) * gamma(n + α + 1)/(gamma(n - k + 1) * gamma(α + k + 1))
-                end
-            else
-                if typeof(α) <: Int
-                    coefficients[k+1] = ((-1)^k)/gamma(k + 1) * binomial(n + α, n - k)
-                else
-                    coefficients[k+1] = ((-1)^k)/gamma(k + 1) * gamma(n + α + 1)/(gamma(n - k + 1) * gamma(α + k + 1))
-                end
-            end
-        end
-        return SVector{n+1}(coefficients)
-    end
+function roots(F::AbstractLaguerre)
+    bound = 2*(2*F.n + 1)
+    return find_zeros(F, (-bound, bound))
 end
-
-"""
-    AssociatedLaguerre(n, α)
-
-Representation of the (n, α) associated Laguerre polynomial. Available methods
-include:
-- `n`: the polynomial degree
-- `α`: the associated parameter 
-- `coefficients`: the polynomial coefficients 
-- `polynomial`: the polynomial function 
-- `weight`: the associated Laguerre polynomial weight function, ``w(x) = x^{\\alpha}e^{-x}``
-- `interval`: the associated Laguerre polynomial orthogonality interval, ``[0, \\infty)``
-
-Note that `AssociatedLaguerre(n, 0)` = `Laguerre(n)`.
-"""
-struct AssociatedLaguerre{T<:Real} <: AbstractLaguerre
-    n::Int 
-    α::T 
-    coefficients
-    polynomial
-    weight
-    interval
-
-    function AssociatedLaguerre(n, α)
-        coefficients = associated_laguerre_coefficients(n, α)
-        polynomial = x -> evalpoly(x, coefficients)
-        weight = x -> (x^α)*exp(-x)
-        interval = (0, Inf)
-        T = promote_type(typeof(α))
-        new{T}(n, α, coefficients, polynomial, weight, interval)
-    end
-end
-
-"""
-    LaguerreLα(n, α, x)
-    LaguerreLα(x; n, α)
-
-Evaluates the (n, α) associated Laguerre polynomial at x ∈ ℝ.
-"""
-function LaguerreLα(n::Int, α::T1, x::T2) where {T1,T2<:Real}
-    return AssociatedLaguerre(n, α).polynomial(x)
-end
-LaguerreLα(x; n, α) = LaguerreLα(n, α, x)
-
-"""
-    Lα(n, α, x)
-    Lα(x; n, α)
-
- Evaluates the (n, α) associated Laguerre polynomial at x ∈ ℝ.
-"""
-Lα(n, α, x) = LaguerreLα(n, α, x)
-Lα(x; n, α) = LaguerreLα(x; n, α)
-
-"""
-    coefficients(L)
-
-Determines the coefficients of a given associated Laguerre polynomial.
-"""
-coefficients(L::AssociatedLaguerre) = L.coefficients 
-
-"""
-    weight(L)
-
-Determines the weight function of a given associated Laguerre polynomial.
-"""
-weight(L::AssociatedLaguerre) = L.weight 
-
-"""
-    interval(L)
-
-Determines the interval of orthogonality of a given associated Laguerre polynomial.
-"""
-interval(L::AssociatedLaguerre) = L.interval
-
-"""
-    innerproduct(L1, L2)
-
-Calculates the inner product of the associated Laguerre polynomials `L1` and `L2`
-using the orthogonality relation
-
-``\\int_0^{\\inftyty} L_m^{(\\alpha)}(x) L_n^{(\\alpha)}(x) x^{\\alpha}e^{-x} \\: \\mathrm{d}x = \\frac{\\Gamma(n + α + 1)}{n!}\\delta_{nm}``
-"""
-function innerproduct(L1::AssociatedLaguerre, L2::AssociatedLaguerre)
-    @assert L1.α == L2.α "Associated Laguerre polynomials must have the same α."
-    n, m = L1.n, L2.n 
-    if n != m 
-        return 0
-    else
-        if n ≤ 20
-            n! = factorial(n)
-        else
-            n! = gamma(n + 1)
-        end
-        return gamma(n + α + 1)/n! 
-    end
-end
-
-# ----------------------------------------------------------------------------------------------------------------------
-# Exports 
-# ----------------------------------------------------------------------------------------------------------------------
-
-export Laguerre 
-export LaguerreL 
-export L 
-export AssociatedLaguerre 
-export LaguerreLα
-export Lα
-export coefficients 
-export weight 
-export interval 
-export innerproduct
 
 # ----------------------------------------------------------------------------------------------------------------------
