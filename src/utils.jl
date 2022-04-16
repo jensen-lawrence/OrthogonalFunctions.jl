6197423bc45122e64a91f50cb329e4d4ae5590b9
Jensen Lawrence <jptlawrnce@gmail.com>
2022-04-15 18:26:20 -0700

    Update utils

--- src/utils.jl
+++ src/utils.jl
@@ -1,12 +1,12 @@
 """
-    genfac(x)
+    fac(x)
 
-Generalized factorial function. Cases:
-- returns `factorial(x)` if 0 ≤ x ≤ 20 and x ∈ ℤ⁺
-- returns `gamma(x + 1)` otherwise
+Generalized factorial function. Returns:
+- `factorial(x)` if 0 ≤ x ≤ 20 and x ∈ ℤ⁺
+- `gamma(x + 1)` otherwise
 """
-function genfac(x::T) where {T<:Real}
-    if (typeof(x) <: Int) && (0 ≤ x ≤ 20)
+function fac(x::T) where {T <: Real}
+    if (typeof(x) <: Integer) && (0 ≤ x ≤ 20)
         return factorial(x)
     else
         return gamma(x + 1)
@@ -14,17 +14,17 @@ function genfac(x::T) where {T<:Real}
 end
 
 """
-    genbinom(x, y)
+    binom(x, y)
 
-Generalized binomial coefficient. Cases:
-- returns `binomial(x, y)` if x, y ∈ ℤ⁺ and 0 ≤ x ≤ 66
-- returns `gamma(x + 1)/(gamma(y + 1) * gamma(x - y + 1))` otherwise 
+Generalized binomial coefficient. Returns:
+- `binomial(x, y)` if x, y ∈ ℤ⁺ and 0 ≤ x ≤ 66 
+- `gamma(x + 1)/(gamma(y + 1) * gamma(x - y + 1))` otherwise
 """
-function genbinom(x::T1, y::T2) where {T1,T2<:Real}
-    if (typeof(x) <: Int) && (typeof(y) <: Int) && (0 ≤ x ≤ 66)
+function binom(x::T1, y::T2) where {T1, T2 <: Real}
+    if (typeof(x) <: Integer) && (typeof(y) <: Integer) && (0 ≤ x ≤ 66)
         return binomial(x, y)
     else
-        return gamma(x + 1)/(gamma(y + 1)*gamma(x - y + 1))
+        return gamma(x + 1)/(gamma(y + 1) * gamma(x - y + 1))
     end
 end
 
@@ -33,10 +33,10 @@ end
 
 Kronecker delta function.
 """
-function δ(n::Int, m::Int)
-    if n == m
-        return 1
-    else
+function δ(n::Integer, m::Integer)
+    if n == m 
+        return 1 
+    else 
         return 0
     end
 end
